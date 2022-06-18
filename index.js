71e75e69171cbb0b7651936f33cf15ee294de4df
RameshNeupane <neupaneramesh555@gmail.com>
2022-06-18 14:29:44 +0545

    update app

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 const path = require("path");
+const favicon = require("serve-favicon");
 
 app.use(express.static("public"));
+app.use(favicon(path.join(__dirname, "public", "static", "favicon.ico")));
 
 app.get("/", (reqeuest, response) => {
   response.sendFile(path.join(__dirname, "public", "static", "index.html"));
